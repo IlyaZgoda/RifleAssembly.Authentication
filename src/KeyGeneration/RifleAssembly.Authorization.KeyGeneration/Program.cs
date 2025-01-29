ad7991735a1f550aba519f32265da9a470404888
IlyaZgoda <zgoda-games@mail.ru>
2025-01-30 01:26:59 +0700

    feat: add key generator and create rsa keys

---
+++ src/KeyGeneration/RifleAssembly.Authorization.KeyGeneration/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace RifleAssembly.Authorization.KeyGeneration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            RsaProvider rsa = new RsaProvider();
+
+            string projectPath = Directory.GetParent(Directory.GetCurrentDirectory())!.Parent!.Parent!.Parent!.Parent!.FullName;
+            string targetSolutionPath = Path.Combine(projectPath, "Web\\RifleAssembly.Authorization.Web\\Keys\\");
+            string privateKeyPath = Path.Combine(targetSolutionPath, "private_key.xml");
+            string publicKeyPath = Path.Combine(targetSolutionPath, "public_key.xml");
+
+            rsa.Generate(privateKeyPath, publicKeyPath);
+        }
+    }
+}
---
+++ src/KeyGeneration/RifleAssembly.Authorization.KeyGeneration/RsaProvider.cs
@@ -0,0 +1,21 @@
+﻿using System.Security.Cryptography;
+
+namespace RifleAssembly.Authorization.KeyGeneration
+{
+    internal class RsaProvider
+    {
+        public void Generate(string privateKeyPath, string publicKeyPath)
+        {
+            using (var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+
+                var privateKey = rsa.ToXmlString(true);
+                File.WriteAllText(privateKeyPath, privateKey);
+
+                var publicKey = rsa.ToXmlString(false);
+                File.WriteAllText(publicKeyPath, publicKey);
+            }
+        }
+    }
+}
