2dea5a068e2ba499fc95aa5b793416506c3ea7c0
IlyaZgoda <zgoda-games@mail.ru>
2025-02-15 21:46:20 +0700

    test

--- src/Web/RifleAssembly.Authentication.Web/Program.cs
+++ src/Web/RifleAssembly.Authentication.Web/Program.cs
@@ -56,7 +56,7 @@ namespace RifleAssembly.Authentication.Web
 
             app.MapGet("/api/health", () => Results.Ok("Healthy!!!!"));
             //app.UseHttpsRedirection();
-
+            //
             string privateKeyXml = Environment.GetEnvironmentVariable("JWT_PRIVATE_KEY");
             if (!string.IsNullOrEmpty(privateKeyXml))
             {
